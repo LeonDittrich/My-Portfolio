19d0278e6212ac215d69c465e6312ee5a5694b6e
LeonDittrich <leon_dittrich@web.de>
2022-05-13 20:08:56 +0200

    route added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
+import { MyWorkComponent } from './my-work/my-work.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    SkillsComponent,
+    MyWorkComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent,
+    ImprintComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/my-work/my-work.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+//import { defaultCipherList } from 'constants'; wirft fehlker wenn an ist
+
+@Component({
+  selector: 'app-my-work',
+  templateUrl: './my-work.component.html',
+  styleUrls: ['./my-work.component.scss']
+})
+export class MyWorkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  
+
+}
