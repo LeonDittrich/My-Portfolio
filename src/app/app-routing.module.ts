67fc9f9f407f1d2b894a954e4d3cb6b2bee2ad15
LeonDittrich <leon_dittrich@web.de>
2022-05-16 19:12:40 +0200

    scroll behavior and add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { MyWorkComponent } from './my-work/my-work.component';
+import { SkillsComponent } from './skills/skills.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '', component: SkillsComponent },
+  { path: '', component: MyWorkComponent },
+  { path: '', component: AboutComponent },
+  { path: '', component: ContactComponent },
+  { path: 'imprint', component: ImprintComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
