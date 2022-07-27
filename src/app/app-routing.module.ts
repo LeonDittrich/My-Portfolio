782256cef5d1d510c75d05abcf385d1d6e166428
LeonDittrich <leon_dittrich@web.de>
2022-07-27 19:08:43 +0200

    add privacy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { SuccessComponent } from './success/success.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'imprint', component: ImprintComponent },
+  { path: 'privacy', component: PrivacyComponent },
   { path: 'success', component: SuccessComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { SuccessComponent } from './success/success.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
 
 @NgModule({
@@ -35,7 +36,8 @@ import { SuccessComponent } from './success/success.component';
     ContactComponent,
     FooterComponent,
     ImprintComponent,
-    SuccessComponent
+    SuccessComponent,
+    PrivacyComponent
   ],
   imports: [
     BrowserModule,
